88cb127f2ac2613345f49a77f34a456a43f9857e
Redcxx <weilue.luo@student.manchester.ac.uk>
2020-01-12 10:53:20 +0000

    remove __init__ from gitignore

---
+++ pikax/__init__.py
@@ -0,0 +1,14 @@
+
+from .api import *
+from .downloader import *
+from .exceptions import *
+from .models import *
+from .params import *
+from .processor import *
+from .result import *
+from .user import *
+from .util import *
+from .pikax import *
+
+name = 'pikax'
+
---
+++ pikax/api/__init__.py
@@ -0,0 +1,7 @@
+
+
+from .androidclient import AndroidAPIClient
+from .artwork import Illust
+from .defaultclient import *
+from .models import *
+from .webclient import *
